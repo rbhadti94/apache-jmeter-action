053ec153045a0265bae83c5e77e0ed0b5d192c1d
Michael Meyers <mrmeyers99@gmail.com>
2021-09-05 11:57:35 -0400

    Test new input variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 # Export JAVA_HOME Variable within Entrypoint
 export JAVA_HOME="/usr/lib/jvm/java-9-openjdk"
-
+echo "dependency folder = ${DEPENDENCY_FOLDER}"
+echo "plugins = ${PLUGINS}"
 jmeter $@
