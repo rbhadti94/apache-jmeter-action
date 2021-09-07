ac3076c0a73bb2c4f917dd5662a68fe814708ef8
Michael Meyers <mrmeyers99@gmail.com>
2021-09-07 08:19:24 -0400

    exit on error

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,14 @@ echo "dependency folder = ${DEPENDENCY_FOLDER}"
 echo "plugins = ${PLUGINS}"
 
 cp ${GITHUB_WORKSPACE}/${DEPENDENCY_FOLDER}/*.jar ${JMETER_HOME}/lib/
+[ $? -eq 0 ]  || exit 1
 
 pluginArray=$(echo $PLUGINS | tr "," "\n")
 for plugin in $pluginArray
 do
   ${JMETER_HOME}/bin/PluginsManagerCMD.sh install $plugin
+  [ $? -eq 0 ]  || exit 1
 done
 
 jmeter $@
+[ $? -eq 0 ]  || exit 1
