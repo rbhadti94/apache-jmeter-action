4a9b28a261c9924874f4eb97d7d303051c57b012
Michael Meyers <mrmeyers99@gmail.com>
2021-09-07 11:22:19 -0400

    check for empty parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,22 @@
 
 # Export JAVA_HOME Variable within Entrypoint
 export JAVA_HOME="/usr/lib/jvm/java-9-openjdk"
-echo "dependency folder = ${DEPENDENCY_FOLDER}"
-echo "plugins = ${PLUGINS}"
 
-cp ${GITHUB_WORKSPACE}/${DEPENDENCY_FOLDER}/*.jar ${JMETER_HOME}/lib/
-[ $? -eq 0 ]  || exit 1
-ls ${JMETER_HOME}/lib
+if [ -n "$DEPENDENCY_FOLDER" ]
+then
+  cp ${GITHUB_WORKSPACE}/${DEPENDENCY_FOLDER}/*.jar ${JMETER_HOME}/lib/
+fi
 
-pluginArray=$(echo $PLUGINS | tr "," "\n")
-for plugin in $pluginArray
-do
-  ${JMETER_HOME}/bin/PluginsManagerCMD.sh install $plugin
-  [ $? -eq 0 ]  || exit 1
-done
+if [ -n "$PLUGINS" ]
+then
+  pluginArray=$(echo $PLUGINS | tr "," "\n")
+  for plugin in $pluginArray
+  do
+    ${JMETER_HOME}/bin/PluginsManagerCMD.sh install $plugin
+    [ $? -eq 0 ] || exit 1
+  done
+fi
 
 jmeter $@
 status=$?
-echo "status=$status"
 [ $status -eq 0 ] || echo "JMeter exited with status code $status" && exit 1
