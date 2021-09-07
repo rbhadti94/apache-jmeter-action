f69e32f8325b6b0cd2950369196b018a3810cf0d
Michael Meyers <mrmeyers99@gmail.com>
2021-09-07 17:11:34 -0400

    do stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,21 +3,21 @@
 # Export JAVA_HOME Variable within Entrypoint
 export JAVA_HOME="/usr/lib/jvm/java-9-openjdk"
 
-#if [ -n "$DEPENDENCY_FOLDER" ]
-#then
-#  cp ${GITHUB_WORKSPACE}/${DEPENDENCY_FOLDER}/*.jar ${JMETER_HOME}/lib/
-#fi
-#
-#if [ -n "$PLUGINS" ]
-#then
-#  pluginArray=$(echo $PLUGINS | tr "," "\n")
-#  for plugin in $pluginArray
-#  do
-#    ${JMETER_HOME}/bin/PluginsManagerCMD.sh install $plugin
-#    [ $? -eq 0 ] || exit 1
-#  done
-#fi
+if [ -n "$DEPENDENCY_FOLDER" ]
+then
+  cp ${GITHUB_WORKSPACE}/${DEPENDENCY_FOLDER}/*.jar ${JMETER_HOME}/lib/
+fi
 
-#jmeter $@
-#status=$?
-#[ $status -eq 0 ] && exit 0 || echo "JMeter exited with status code $status" && exit $status
+if [ -n "$PLUGINS" ]
+then
+  pluginArray=$(echo $PLUGINS | tr "," "\n")
+  for plugin in $pluginArray
+  do
+    ${JMETER_HOME}/bin/PluginsManagerCMD.sh install $plugin
+    [ $? -eq 0 ] || exit 1
+  done
+fi
+
+jmeter $@
+status=$?
+[ $status -eq 0 ] && exit 0 || echo "JMeter exited with status code $status" && exit $status
