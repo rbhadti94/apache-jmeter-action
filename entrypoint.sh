93a6036a8bb96470ca12f3c54ef32bf86db0b5e4
Michael Meyers <mrmeyers99@gmail.com>
2021-09-07 07:56:29 -0400

    install plugins, make dep folder relative to github_workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,12 @@ export JAVA_HOME="/usr/lib/jvm/java-9-openjdk"
 echo "dependency folder = ${DEPENDENCY_FOLDER}"
 echo "plugins = ${PLUGINS}"
 
-cp ${DEPENDENCY_FOLDER}/*.jar ${JMETER_HOME}/lib/
+cp ${GITHUB_WORKSPACE}/${DEPENDENCY_FOLDER}/*.jar ${JMETER_HOME}/lib/
+
+pluginArray=$(echo $PLUGINS | tr "," "\n")
+for plugin in $pluginArray
+do
+  ${JMETER_HOME}/bin/PluginsManagerCMD.sh install $plugin
+done
 
 jmeter $@
