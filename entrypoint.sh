bfd3558b886668a08c500f23b60e5a2ab05a45b3
Michael Meyers <mrmeyers99@gmail.com>
2021-09-08 09:41:36 -0400

    add parallel

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,6 @@ fi
 if [ -n "$PLUGINS" ]
 then
   echo "$PLUGINS" | tr "," "\n" | parallel -I% --jobs 5 "${JMETER_HOME}/bin/PluginsManagerCMD.sh install %"
-#  for plugin in $pluginArray
-#  do
-#    ${JMETER_HOME}/bin/PluginsManagerCMD.sh install $plugin
-#    [ $? -eq 0 ] || exit 1
-#  done
 fi
 
 jmeter $@
