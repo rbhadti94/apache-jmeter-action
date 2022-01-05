0576dfaae22c42e81b01ddaff000b4893024f9cb
Ravindra Bhadti <rbhadti@gmail.com>
2022-01-05 22:51:17 +0000

    GH-21 Multiple JMX Files (#22)
    * multiple entrypoint
    
    * Fixing log4j vuln

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM alpine:3.12.1
+FROM alpine:3.15
 
-LABEL "maintainer" "Ravindra Bhadti"
+LABEL "maintainer" "Ravindra Bhadti, @rbhadti94"
 LABEL "com.github.actions.name"="apache-jmeter"
 LABEL "com.github.actions.description"="Run Apache JMeter Performance Tests"
 
-ENV JMETER_VERSION "5.4.1"
+ENV JMETER_VERSION "5.4.3"
 ENV JMETER_HOME "/opt/apache/apache-jmeter-${JMETER_VERSION}"
 ENV JMETER_BIN "${JMETER_HOME}/bin"
 ENV PATH "$PATH:$JMETER_BIN"
@@ -15,7 +15,7 @@ ENV MAVEN_REPO "https://repo1.maven.org/maven2/kg/apc"
 COPY entrypoint.sh /entrypoint.sh
 COPY cleanup.sh /cleanup.sh
 
-RUN apk --no-cache add curl ca-certificates openjdk9-jre parallel && \
+RUN apk --no-cache add curl ca-certificates openjdk9-jre parallel bash && \
     curl -L https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-${JMETER_VERSION}.tgz --output /tmp/apache-jmeter-${JMETER_VERSION}.tgz && \
     tar -zxf /tmp/apache-jmeter-${JMETER_VERSION}.tgz && \
     mkdir -p /opt/apache && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+TESTFILE_PATH=$3
+
+echo "Using Test File Path $TESTFILE_PATH"
+
+# Removing first 3 arguments from Entrypoint. Need to find a better way to do this
+set -- "${@:1:0}" "${@:2}"
+set -- "${@:1:0}" "${@:2}"
+set -- "${@:1:0}" "${@:2}"
+
+echo $@
 
 # Export JAVA_HOME Variable within Entrypoint
 export JAVA_HOME="/usr/lib/jvm/java-9-openjdk"
@@ -13,6 +24,31 @@ then
   echo "$PLUGINS" | tr "," "\n" | parallel -I% --jobs 5 "${JMETER_HOME}/bin/PluginsManagerCMD.sh install %"
 fi
 
-jmeter $@
-status=$?
+status=0
+
+if [[ $TESTFILE_PATH == *.jmx ]]
+then
+  echo "Single file specified so only running one test"
+  echo "Running jmeter -n -t $TESTFILE_PATH $@"
+  jmeter -n -t $TESTFILE_PATH $@
+  status=$?
+else
+  BASEFILE_PATH=$(basename $TESTFILE_PATH)
+  echo "Folder specified - Running each JMX File In Folder"
+  for FILE in $BASEFILE_PATH/*.jmx
+  do
+    echo "Running test with $FILE"
+    jmeter -n -t $FILE $@
+    test_run=$?
+    # If any of the previous tests haven't failed
+    if [ "$test_run" == "0" ] && [ "$status" == "1" ]
+    then
+      status=1 # Set one of the tests failing
+    fi
+    echo "Test $FILE has exited with status code $test_run"
+  done
+fi
+
+error=0 # Default error status code
+
 [ $status -eq 0 ] && exit 0 || echo "JMeter exited with status code $status" && exit $status
